7706d6aa85c68369372968c169a974c2f5498c8b
Ruan Costa Oliveira <umenorin@gmail.com>
2025-07-27 14:50:47 -0300

    adding new text for about page

--- src/app/features/about/AboutTraslation.ts
+++ src/app/features/about/AboutTraslation.ts
@@ -2,7 +2,7 @@ const AboutTranslation = {
   pt_br: {
     title: "Sobre mim",
     my_description:
-      "Designer e desenvolvedora do Rio de Janeiro com foco em criar soluções digitais que unem estética, funcionalidade e tecnologia. Minha trajetória é dedicada a explorar a sinergia entre o design criativo e o desenvolvimento técnico, atuando de ponta a ponta no ciclo de vida de um produto. Formada em Design Digital (PUC-Rio), pós-graduada em UI/UX (Unyleya) e atualmente cursando Análise e Desenvolvimento de Sistemas (Mackenzie).",
+      "Desenvolvedor full-stack com foco em criar soluções digitais completas, eficientes e com propósito. Minha trajetória é voltada à construção de produtos que aliam design funcional, performance e uma experiência de uso inteligente. Atuo em todas as etapas do desenvolvimento, desde a interface até a lógica de negócio, sempre buscando escalabilidade, organização e boas práticas. Atualmente, curso Sistemas de Informação na UVV, onde aprofundo meus conhecimentos em desenvolvimento de software, arquitetura de sistemas e tecnologias modernas aplicadas ao dia a dia da programação.",
     subtitle_social_media: "Redes Sociais",
     subtitle_graduation: "Graduação",
     subtitle_interests: "Interesses",
@@ -11,15 +11,15 @@ const AboutTranslation = {
     obs_text: "Arraste para o lado para ver mais opções",
 
     description_interests:
-      "Designer e desenvolvedora do Rio de Janeiro com foco em criar soluções digitais que unem estética, funcionalidade e tecnologia. Minha trajetória é dedicada a explorar a sinergia entre o design criativo e o desenvolvimento técnico, atuando de ponta a ponta no ciclo de vida de um produto. Formada em Design Digital (PUC-Rio), pós-graduada em UI/UX (Unyleya) e atualmente cursando Análise e Desenvolvimento de Sistemas (Mackenzie).",
+      "Tenho grande interesse em explorar temas como desenvolvimento full-stack e arquitetura de software, buscando entender como estruturar aplicações robustas e escaláveis. Tenho curiosidade por áreas como segurança da informação, criação de APIs e microsserviços, além de querer me aprofundar em iniciativas Open Source e no uso de inteligência artificial aplicada ao desenvolvimento. Também me interesso por tecnologias emergentes, como o WebAssembly, e pretendo estudá-las com mais profundidade para expandir minhas possibilidades como desenvolvedor.",
     description_Hobbies:
-      "Designer e desenvolvedora do Rio de Janeiro com foco em criar soluções digitais que unem estética, funcionalidade e tecnologia. Minha trajetória é dedicada a explorar a sinergia entre o design criativo e o desenvolvimento técnico, atuando de ponta a ponta no ciclo de vida de um produto. Formada em Design Digital (PUC-Rio), pós-graduada em UI/UX (Unyleya) e atualmente cursando Análise e Desenvolvimento de Sistemas (Mackenzie).",
+      "Nos momentos livres, gosto de jogar, explorar boas leituras e me aprofundar em temas ligados à programação, que além de profissão, também é um dos meus maiores interesses pessoais. Sempre que possível, busco aprender algo novo ou experimentar tecnologias por curiosidade. Futuramente, pretendo incluir a guitarra na rotina, como uma forma de relaxar e estimular a criatividade por outros caminhos.",
   },
 
   en_us: {
     title: "About me",
     my_description:
-      "Designer e desenvolvedora do Rio de Janeiro com foco em criar soluções digitais que unem estética, funcionalidade e tecnologia. Minha trajetória é dedicada a explorar a sinergia entre o design criativo e o desenvolvimento técnico, atuando de ponta a ponta no ciclo de vida de um produto. Formada em Design Digital (PUC-Rio), pós-graduada em UI/UX (Unyleya) e atualmente cursando Análise e Desenvolvimento de Sistemas (Mackenzie).",
+      "Full-stack developer , focused on creating complete, efficient, and purposeful digital solutions. My journey is dedicated to building products that combine functional design, performance, and an intelligent user experience. I work across all stages of development, from interface to business logic, always striving for scalability, organization, and best practices. Currently, I am pursuing a degree in Information Systems at UVV, where I deepen my knowledge in software development, system architecture, and modern technologies applied to everyday programming.",
     subtitle_social_media: "Social Media",
     subtitle_graduation: "Graduation",
     subtitle_interests: "Interests",
@@ -28,9 +28,9 @@ const AboutTranslation = {
     obs_text: "Swipe to see more options",
 
     description_interests:
-      "Designer e desenvolvedora do Rio de Janeiro com foco em criar soluções digitais que unem estética, funcionalidade e tecnologia. Minha trajetória é dedicada a explorar a sinergia entre o design criativo e o desenvolvimento técnico, atuando de ponta a ponta no ciclo de vida de um produto. Formada em Design Digital (PUC-Rio), pós-graduada em UI/UX (Unyleya) e atualmente cursando Análise e Desenvolvimento de Sistemas (Mackenzie).",
+      "I have a strong interest in exploring topics like full-stack development and software architecture, seeking to understand how to structure robust and scalable applications. I’m curious about areas such as information security, API and microservices development, and I also want to dive deeper into Open Source initiatives and the use of AI in programming. Additionally, I’m fascinated by emerging technologies like WebAssembly and plan to study them in more depth to expand my possibilities as a developer.",
     description_Hobbies:
-      "Designer e desenvolvedora do Rio de Janeiro com foco em criar soluções digitais que unem estética, funcionalidade e tecnologia. Minha trajetória é dedicada a explorar a sinergia entre o design criativo e o desenvolvimento técnico, atuando de ponta a ponta no ciclo de vida de um produto. Formada em Design Digital (PUC-Rio), pós-graduada em UI/UX (Unyleya) e atualmente cursando Análise e Desenvolvimento de Sistemas (Mackenzie).",
+      "In my free time, I enjoy gaming, diving into good books, and exploring programming related topics which, beyond being my profession, is also one of my greatest personal interests. Whenever possible, I try to learn something new or experiment with technologies out of sheer curiosity. In the future, I plan to pick up the guitar as a way to unwind and spark creativity through a different medium.",
   },
 };
 
