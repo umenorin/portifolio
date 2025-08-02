8e3458882ba736359ca6e1bec178de3c2b30c196
Ruan Costa Oliveira <umenorin@gmail.com>
2025-08-02 15:57:48 -0300

    applying the CV in english

--- src/app/features/about/components/AboutMeSection/components/DownloadButton/DownloadButton.tsx
+++ src/app/features/about/components/AboutMeSection/components/DownloadButton/DownloadButton.tsx
@@ -5,6 +5,7 @@ import "./DownloadButton.scss";
 interface PersonalStatusProp {
   text: string;
   url: string;
+  download?: boolean | string;
 }
 
 const DownloadButton: Component<PersonalStatusProp> = (props) => {
@@ -21,23 +22,17 @@ const DownloadButton: Component<PersonalStatusProp> = (props) => {
   });
 
   return (
-    <a class="personal-status__image" href={props.url} download>
-      <div class="personal-status__container">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height={`${width() > 450 ? "24px" : "20button"}`}
-          viewBox="0 -960 960 960"
-          width={`${width() > 450 ? "24px" : "20"}`}
-          fill="#f2f2f2"
-        >
-          <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" />
-        </svg>
-        <p
-          class={`personal-status__paragraph`}
-        >
-          {props.text}
-        </p>
-      </div>
+    <a class="download-button__container" href={props.url} download="">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height={`${width() > 450 ? "24px" : "20button"}`}
+        viewBox="0 -960 960 960"
+        width={`${width() > 450 ? "24px" : "20"}`}
+        fill="#f2f2f2"
+      >
+        <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" />
+      </svg>
+      <p class={`download-button__paragraph`}>{props.text}</p>
     </a>
   );
 };
